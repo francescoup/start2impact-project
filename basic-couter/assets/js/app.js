57fc255b39389d1f1df53a330063750766b456ae
francescoup <94630119+francescoup@users.noreply.github.com>
2022-01-24 19:33:09 +0000

    add project
    project basic counter

---
+++ basic-couter/assets/js/app.js
@@ -0,0 +1,63 @@
+// body selector
+
+let counterDisplay = document.querySelector('h1');
+let counterAdd = document.querySelector('.counter-up');
+let counterLess = document.querySelector('.counter-less');
+let showReset = document.querySelector('.show-reset');
+
+let count = 0;
+
+displayReset()
+displayUpgrade();
+displayUp()
+displayless()
+reset()
+
+// counter events
+
+showReset.addEventListener('click', ()=> {
+  reset();
+  displayReset();
+
+})
+
+counterAdd.addEventListener('click', ()=> {
+  count++;
+  displayUpgrade()
+  displayReset()
+})
+
+counterLess.addEventListener('click', ()=> {
+  count--;
+  displayUpgrade()
+  displayReset()
+})
+
+// functions for upgrade display
+
+function displayUpgrade(){
+  counterDisplay.innerHTML = count;
+}
+
+function displayUp(){
+  counterAdd.innerHTML = '+';
+}
+function displayless(){
+  counterLess.innerHTML = '-';
+}
+
+// functions for show reset button and reset display
+
+function displayReset(){
+  if(count!=0 ){
+    showReset.style.top='-10px';
+  }
+  else {
+    showReset.style.top='-100px';
+  }
+}
+
+function reset (){
+  count = 0;
+  counterDisplay.innerHTML = count;
+}
